ed0dd9d3d5859a4b104a0098b0b20cedd576ca2a
Daniel Hernandez <daniel244@live.com.mx>
2023-03-10 09:06:35 -0600

    create graphql-yoga base server

---
+++ graphql-yoga/src/models/Employee.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const employeeSchema = new Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    age: {
+        type: Number
+    },
+    position: {
+        type: String
+    },
+    code: {
+        type: String
+    }
+});
+export default model('Employee', employeeSchema)
