4881f4a6bced61fc6e2a8e7ee2174138b7963667
Daniel Hernandez <daniel244@live.com.mx>
2023-03-10 21:24:12 -0600

    build employee mutations

--- graphql-yoga/src/database.js
+++ graphql-yoga/src/database.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose"
 
+///------ conectar mongodb localmente ----------- //
+
 export async function connect(){
     try {
         await mongoose.connect("mongodb://127.0.0.1/mongodbgraphql", {
@@ -10,3 +12,12 @@ export async function connect(){
         return console.log('Something went wrong!!!', error);
     }
 }
+
+///------ conectar con mongo atlas ----------- //
+
+// const uri = "mongodb+srv://iamdanihdz:salmos.8318@cluster0.ct7iedv.mongodb.net/?retryWrites=true&w=majority"
+// mongoose.connect( uri, {
+//     useNerUrlParser: true,
+//     useUnifiedTopology: true
+// }).then( (db) => console.log('DB connected'))
+// .catch(err => console.log(err))
--- graphql-yoga/src/index.js
+++ graphql-yoga/src/index.js
@@ -1,6 +1,11 @@
 import server from "./server.js"
+
+//------conexión hacia mongo local
 import { connect } from "./database.js";
 connect();
+
+//----conexión hacia mongo atlas
+//import "./database.js"
 
 server.start( { port: 3000 }, ( { port } ) => {
     console.log('Sever is running on' + port )
