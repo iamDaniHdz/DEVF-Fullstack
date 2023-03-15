7450abf302c243d8b47d89a0b6e87bbb0a2ec101
Daniel Hernandez <daniel244@live.com.mx>
2023-03-14 22:16:44 -0600

    wrapping up the yoga backend

--- graphql-yoga/src/graphql/Query.js
+++ graphql-yoga/src/graphql/Query.js
@@ -1,9 +1,22 @@
 import Employee from "../models/Employee.js"
+import User from "../models/User.js"
 
 const Query = {
     async getEmployees() {
         const employees = await Employee.find();
         return employees
+    },
+
+    async login(_,{email,password} ) {
+        const verifyUser = await User.findOne( { email, password } ); //[]
+        console.log(verifyUser)
+        return verifyUser
+    },
+
+    async getEmployeeByName( _, { name } ) {
+        const searchEmployeeByName = await Employee.find( { name } );
+        console.log( 'getting employee', searchEmployeeByName );
+        return searchEmployeeByName 
     }
 }
 
