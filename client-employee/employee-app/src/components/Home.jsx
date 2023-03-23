1a635c49e268b3644cf5593adc5b4287667566b2
Daniel Hernandez <daniel244@live.com.mx>
2023-03-23 09:53:12 -0600

    create zustand store for user sessions

--- client-employee/employee-app/src/components/Home.jsx
+++ client-employee/employee-app/src/components/Home.jsx
@@ -3,18 +3,21 @@ import { useLazyQuery, useMutation } from "@apollo/client";
 import { GET_EMPLOYEE } from "../graphql/Queries";
 import { REMOVE_EMPLOYEE } from "../graphql/Mutation";
 import { Link } from "react-router-dom";
+import { userState } from "../config/UserState";
 
 export const Home = () => {
 
-	const [searchEmployee, { data, error }] = useLazyQuery( GET_EMPLOYEE );
+	const [searchEmployee, { data, error }] = useLazyQuery(GET_EMPLOYEE);
+	const verifySession = userState((state) => state.session);
+	console.log("session from home", verifySession);
 
-	const [removeEmployee] = useMutation( REMOVE_EMPLOYEE, {
+	const [removeEmployee] = useMutation(REMOVE_EMPLOYEE, {
 		//Ejecutar el query después de eliminar el empleado de la base de datos
-		refetchQueries: [{query: GET_EMPLOYEE}]
-	})
+		refetchQueries: [{ query: GET_EMPLOYEE }],
+	});
 
-	useEffect( () => {
-		console.log('use effect in home')     
+	useEffect(() => {
+		console.log("use effect in home");
 		searchEmployee();
 	}, []);
 
@@ -28,11 +31,12 @@ export const Home = () => {
 		<div className="flex gap-4 pt-4">
 			{data &&
 				data.getEmployees.map(({ _id, name, position, code, age }) => (
-					<Link
-						to="/create-employee"
-						state= {{_id,name,code,position,age}}
-						className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-
+					<>
+						<Link
+							to="/create-employee"
+							state={{ _id, name, code, position, age }}
+							className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+						>
 						<a href="#">
 							<img
 								className="rounded-t-lg"
@@ -41,30 +45,33 @@ export const Home = () => {
 							/>
 						</a>
 						<div className="p-5">
-							<a href="#">
-								<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-									{name}
-								</h5>
-							</a>
-							<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-								{age}
-							</p>
-							<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-								{position}
-                            </p>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-								{code}
-							</p>
+								<a href="#">
+									<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+										{name}
+									</h5>
+								</a>
+								<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+									{position}
+								</p>
+								<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+									{code}
+								</p>
+							</div>
+							</Link>
+						<div>
+							<button
+								onClick={async (e) => {
+									await removeEmployee({
+										variables: { _id },
+									});
+								}}
+								type="button"
+								class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300  font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+							>
+								Remove
+							</button>
 						</div>
-						<button
-							onClick={async ( e ) => {
-								await removeEmployee( {
-									variables: {_id}
-								})
-							}}
-							type="button"
-							class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Remove</button>
-					</Link>
+					</>
 				))}
 		</div>
 	);
--- client-employee/employee-app/src/components/Login.jsx
+++ client-employee/employee-app/src/components/Login.jsx
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import { LOGIN } from "../graphql/Queries";
 import { useLazyQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
+import {userState} from "../config/UserState"
 
 export const Login = () => {
 
     const navigate = useNavigate()
     const [email, setEmail] = useState( "" );
     const [password, setPassword] = useState( "" );
-    const [isInvalid, setIsInvalid] = useState("")
-
+    const [isInvalid, setIsInvalid] = useState( "" );
+    const setUserSession =  userState( (state) => state.addSession)
+    const verifySession = userState( ( state ) => state.session )
+    console.log(verifySession)
     const [login, { data, error }] = useLazyQuery( LOGIN, {
         variables : {email,password}
     })
@@ -22,11 +25,9 @@ export const Login = () => {
 
                     var data = response.data.login
                     console.log( data )
-
                     if ( data ) {
+                        setUserSession({isValid : true})
                         navigate( '/home' )
-                        //SETEAR EL ESTADO DE EL USUARIO
-
                     } else {
                         setIsInvalid('Invalid Credential!!')
                     }
